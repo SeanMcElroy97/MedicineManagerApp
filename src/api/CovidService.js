2e24b474d1f565ff51494f9b524b061450874852
SeanMcelroy97 <seanpetermcelroy@gmail.com>
2020-04-13 19:29:56 +0100

    Added CountUp js library to add a better visual for numbers displayed

--- src/api/CovidService.js
+++ src/api/CovidService.js
@@ -4,17 +4,25 @@ import Axios from "axios";
 class CovidService {
   executeTest() {
     //returns a PromisObj
-    return Axios.get(URLwithID + "/covid19/covyTest")
+    return Axios.get(URL + "/covid19/covyTest")
       .then(response => console.log(response))
       .catch(alert("couldnt get reach test"));
   }
 
-  // retrieveAllCovidData() {
-  //   console.log(URLwithID);
-  //   return Axios.get(URLwithID + "/covid19/all");
-  // }
+  executeTest2 = async () => {
+    try {
+      const response = await Axios.get(URL + "/covid19/covyTest");
+      return response;
+    } catch (error) {
+
+    }
+  }
+
+  retrieveGlobalTotals() {
+    return Axios.get(URL + "/covid19/globalTotals");
+  }
 
 
 }
 
-export default new MedicineService();
+export default new CovidService();
--- src/components/covid19/Covid19MainComponent.jsx
+++ src/components/covid19/Covid19MainComponent.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import CovidService from "../../api/CovidService.js";
+import CountUp from 'react-countup';
+
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 class Covid19MainComponent extends React.Component {
+
+
+    state = {
+        covid19GlobalCases: 0
+
+    }
+
+
+    async componentDidMount() {
+        CovidService.retrieveGlobalTotals()
+            .then(response => this.setState({ covid19GlobalCases: response.data.totalConfirmed }));
+        // .then(response => console.log(response));
+
+    }
+
+
     render() {
         return (
-            <div>Test covid Component</div>
+            <div className="container">
+                <Grid container spacing={3} justift="center">
+                    <Grid item component={Card}>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
+                            <Typography variant="h5">
+                                <CountUp
+                                    start={0}
+                                    end={this.state.covid19GlobalCases}
+                                    duration={2.5}
+                                    separator=","
+                                />
+                            </Typography>
+                            <Typography color="textSecondary">REAL DATE 02:00 AM</Typography>
+                            <Typography variant="body2">Number of confirmed Cases of COVID-19</Typography>
+                        </CardContent>
+                    </Grid>
+                    <Grid item component={Card}>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                            <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+                            <Typography color="textSecondary">REAL DATE 02:00 AM</Typography>
+                            <Typography variant="body2">Number of COVID-19 Deaths</Typography>
+                        </CardContent>
+                    </Grid>
+                    <Grid item component={Card}>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                            <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+                            <Typography color="textSecondary">REAL DATE 02:00 AM</Typography>
+                            <Typography variant="body2">Number of COVID-19 Recovories</Typography>
+                        </CardContent>
+                    </Grid>
+                </Grid>
+            </div>
         )
     }
+
 }
 
 export default Covid19MainComponent;
